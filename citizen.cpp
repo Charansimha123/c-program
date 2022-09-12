ae53d70092925b613ea6effb283724f26e0fc522
Charansimha123 <113327024+Charansimha123@users.noreply.github.com>
2022-09-12 11:18:05 +0530

    Add files via upload

---
+++ citizen.cpp
@@ -0,0 +1,15 @@
+Enter the principal amount: 200000
+Enter the no of years: 3
+Is customer senior citizen (y/n): n
+Sample Output:
+Interest: 60000
+#include<iostream>
+using namespace std;
+int main()
+{
+int p,y,c,i,a;
+cout<<"enter the principle amount => ";
+cin>>p;
+cout<<"\nenter the no of years => ";
+cin>>y;
+cout<<"\n1.senior citizen\n2.not senio
