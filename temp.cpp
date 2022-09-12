66fe00e390e5fef7381a723526df11046a6576cb
Charansimha123 <113327024+Charansimha123@users.noreply.github.com>
2022-09-12 11:05:01 +0530

    Add files via upload

---
+++ temp.cpp
@@ -0,0 +1,19 @@
+for(j=0;j<n-i;j++)
+cout<<" ";
+temp=1;
+for(k=0;k<=i;k++)
+{
+cout<<temp<<" ";
+temp=temp*(i-k)/(for(j=0;j<n-i;j++)
+cout<<" ";
+temp=1;
+for(k=0;k<=i;k++)
+{
+cout<<temp<<" ";
+temp=temp*(i-k)/(for(j=0;j<n-i;j++)
+cout<<" ";
+temp=1;
+for(k=0;k<=i;k++)
+{
+cout<<temp<<" ";
+temp=temp*(i-k)/(
